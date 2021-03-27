336adc0074a3a6d3d40abe3dad015cef56552009
Deniz Akşimşek <deniz@denizaksimsek.com>
2021-03-27 18:41:03 +0000

    more

--- prism-hyperscript.js
+++ prism-hyperscript.js
@@ -2,14 +2,20 @@
 (Prism => {
 	Prism.languages.hyperscript = {
 		'comment': /\-\-.*/,
-		'punctuation': /[(){}[\],:]/,
+		'punctuation': /[(){}[\],:;\?&]/,
 		'keyword': {
-			pattern: /\b(?:on|def|js|worker|eventsource|socket|init|add|async|call|get|fetch|hide|measure|if|js|log|put|remove|repeat forever|repeat for|repeat in|repeat while|repeat until event|repeat until|repeat|return|send|settle|set|show|take|throw|toggle|transition|trigger|wait for|wait|tell|go|then|while|until|for|in|from|to|with|over)\b/g,
+			pattern: /\b(?:on|def|js|worker|eventsource|socket|init|add|async|call|get|fetch|hide|measure|if|js|log|put|remove|repeat\sforever|repeat\sfor|repeat\sin|repeat\swhile|repeat\suntil\sevent|repeat until|repeat|return|send|settle|set|show|take|throw|toggle\sbetween|toggle|transition|trigger|wait for|wait|tell|go|then|end|while|until|for|in|from|to|with|over)\b/g,
 			inside: {
-				'bold': /\b(?:on|def|js|worker|eventsource|socket|init|add|async|call|get|fetch|hide|measure|if|js|log|put|remove|repeat forever|repeat for|repeat in|repeat while|repeat until event|repeat until|repeat|return|send|set|settle|show|take|throw|toggle|transition|trigger|wait for|wait|tell|go)\b/g
+				'marker': {
+					pattern: /\b(?:on|def|js|worker|eventsource|socket|init|add|async|call|get|fetch|hide|measure|if|js|log|put|remove|repeat\sforever|repeat\sfor|repeat\sin|repeat\swhile|repeat\suntil\sevent|repeat\suntil|repeat|return|send|set|settle|show|take|throw|toggle\sbetween|toggle|transition|trigger|wait for|wait|tell|go|end|for)\b/g,
+					alias: 'bold',
+				}
 			}
 		},
-		'operator': /\+|-|\/|\*|<\b|>|<=|>=|\b(?:is|am|as|and|or|no|closest|the|of|first|last|on|is\sa|is\san)\b/,
+		'operator': {
+			pattern: /\+|-|\/|\*|<\s|>|<=|>=|==|===|!=|!==|=|\.\.|(\w)\.|%|\||!|$|\b()(?:is|am|as|and|or|no|closest|the|of|first|last|on|is\sa|is\san|s|ms|seconds|milliseconds)\b/,
+			lookbehind: true,
+		},
 		'builtin': /\b(?:I|me|my|it|its|result|event|target|detail)\b/,
 		'function': /[-A-Za-z0-9]+(?=\()/i,
 		'class-ref': {
@@ -17,7 +23,7 @@
 			alias: 'selector'
 		},
 		'id-ref': {
-			pattern: /\s#[-\w\d_\$]\b/,
+			pattern: /\s#[-\w\d_\$]+\b/,
 			alias: 'selector'
 		},
 		'selector': /<[^\s].*\/>/,
@@ -26,12 +32,25 @@
 			pattern: /("[^\n]*[^\\]?"|'[^\n]*[^\\]?')/,
 			greedy: true,
 		},
+		'number': /(\d+\.?|\d*\.\d+)(s|ms)?/,
 		'hs-template-literal': {
 			pattern: /`[^\n`]*`/,
 			greedy: true,
 			inside: {
-				'string': /`.*\$\{|`.*`|\}*\$\{|\}.*`/
+				'string': {
+					pattern: /`\$\{|}\$\{|}`|`.*[^\\]\$\{|`.*`|\}.*[^\\]\$\{|\}.*`/,
+					inside: {
+						operator: /^}|\$\{$/
+					}
+				}
 			}
 		}
+	}
+
+	// `
+
+	if (Prism.languages.markup) {
+		Prism.languages.markup.tag.addInlined('script', 'hyperscript')
+		Prism.languages.markup.tag.addAttribute('_', 'hyperscript')
 	}
 })(Prism)
