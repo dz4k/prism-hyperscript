0ade84b91c7a67994323e7e3f0cf950f606fbd13
Deniz Akşimşek <deniz@denizaksimsek.com>
2021-04-06 18:04:30 +0000

    fixes

--- prism-hyperscript.js
+++ prism-hyperscript.js
@@ -9,17 +9,23 @@
 	Object.assign(Prism.languages.hyperscript, {
 		'comment': /\-\-.*/,
 		'punctuation': /[(){}[\],:;\?&]/,
+		'url': {
+			pattern: /(fetch\s)[^`"'\s][^\s]*/, // `
+			lookbehind: true,
+			greedy: true,
+		},
 		'keyword': {
-			pattern: /\b(?:on|def|js|worker|eventsource|socket|init|behavior|install|add|async|call|get|fetch|hide|measure|if|js|log|put|remove|repeat\sforever|repeat\sfor|repeat\sin|repeat\swhile|repeat\suntil\sevent|repeat until|repeat|return|send|settle|set|show|take|throw|toggle\sbetween|toggle|transition|trigger|wait for|wait|tell|go|then|end|while|until|for|in|from|to|with|over|into|before|after|at end of|at start of|is an|is a|am|as|and|or|no|closest|the|of|first|last|on|seconds|milliseconds|s|ms)\b/g,
+			pattern: /\b(?:on|def|js|worker|eventsource|socket|init|behavior|install|catch|add|async|call|get|hide|measure|if|js|log|put|remove|repeat\sforever|repeat\sfor|repeat\sin|repeat\swhile|repeat\suntil\sevent|repeat until|repeat|return|send|settle|set|show|take|throw|toggle\sbetween|toggle|transition|trigger|wait for|wait|fetch|tell|go|then|end|while|until|for|in|from|to|with|over|into|before|after|at end of|at start of|is an|is a|am|as|and|or|no|closest|the|of|first|last|on|seconds|milliseconds|(\s)s|(\s)ms)\b/g,
+			lookbehind: true,
 			inside: {
 				'hs-start': {
-					pattern: /\b(?:on|def|js|worker|eventsource|socket|init|behavior|install|add|async|call|get|fetch|hide|measure|if|js|log|put|remove|repeat\sforever|repeat\sfor|repeat\sin|repeat\swhile|repeat\suntil\sevent|repeat\suntil|repeat|return|send|set|settle|show|take|throw|toggle\sbetween|toggle|transition|trigger|wait for|wait|tell|go|end|for)\b/g,
+					pattern: /\b(?:on|def|js|worker|eventsource|socket|init|behavior|install|catch|add|async|call|get|hide|measure|if|js|log|put|remove|repeat\sforever|repeat\sfor|repeat\sin|repeat\swhile|repeat\suntil\sevent|repeat\suntil|repeat|return|send|set|settle|show|take|throw|toggle\sbetween|toggle|transition|trigger|wait for|wait|fetch|tell|go|end|for)\b/g,
 					alias: 'bold',
 				}
 			}
 		},
 		'operator': {
-			pattern: /\+|\s-\s|\/|\*|\\|<\s|>|<=|>=|==|===|!=|!==|=|\.\.|(\w)\.|\%|\||!|\$/,
+			pattern: /\+|\s-\s|\/|\*|\\|->|<\s|>|<=|>=|==|===|!=|!==|=|\.\.|([^\d\s]|^)\.|\%|\||!|\$|'s/, // '
 			lookbehind: true,
 		},
 		'builtin': /\b(?:I|me|my|it|its|result|event|target|detail)\b/,
@@ -40,10 +46,13 @@
 		},
 		'boolean': /\b(?:true|false|null)\b/,
 		'string': {
-			pattern: /("[^\n]*[^\\]?"|'[^\n]*[^\\]?')/,
+			pattern: /"[^\n"]*"|'[^\n']*'/, // "
 			greedy: true,
 		},
-		'number': /(\d+\.?|\d*\.\d+)(s|ms)?/,
+		'number': {
+			pattern: /(\d+\.?|\d*\.\d+)(s|ms)?/,
+			greedy: true,
+		},
 		'template-string': {
 			pattern: /`[^\n`]*`/, // ` //
 			greedy: true,
