75d195879a8a1a441aefeed9c106e0619da4f556
Deniz Akşimşek <deniz@denizaksimsek.com>
2021-04-22 10:41:22 +0000

    fix selector precedence

--- prism-hyperscript.js
+++ prism-hyperscript.js
@@ -14,6 +14,20 @@
 			lookbehind: true,
 			greedy: true,
 		},
+		'class-ref': {
+			pattern: /\s\.[\-\w\d_\$]+/,
+			alias: 'selector',
+			greedy: true
+		},
+		'id-ref': {
+			pattern: /#[\-\w\d_\$]+/,
+			alias: 'selector',
+			greedy: true
+		},
+		'selector': {
+			pattern: /<[^\s].*\/>/,
+			greedy: true,
+		},
 		'keyword': {
 			pattern: /\b(?:on|def|js|worker|eventsource|socket|init|behavior|install|catch|add|async|call|get|hide|measure|if|js|log|put|remove|repeat\sforever|repeat\sfor|repeat\sin|repeat\swhile|repeat\suntil\sevent|repeat until|repeat|return|send|settle|set|show|take|throw|toggle\sbetween|toggle|transition|trigger|wait for|wait|fetch|tell|go|then|end|while|until|for|in|from|to|with|over|into|before|after|at end of|at start of|is an|is a|am|as|and|or|no|closest|the|of|first|last|on|seconds|milliseconds|(\s)s|(\s)ms)\b/g,
 			lookbehind: true,
@@ -30,20 +44,6 @@
 		},
 		'builtin': /\b(?:I|me|my|it|its|result|event|target|detail)\b/,
 		'function': /[A-Za-z0-9]+(?=\()/,
-		'class-ref': {
-			pattern: /\s\.[\-\w\d_\$]+/,
-			alias: 'selector',
-			greedy: true
-		},
-		'id-ref': {
-			pattern: /#[\-\w\d_\$]+/,
-			alias: 'selector',
-			greedy: true
-		},
-		'selector': {
-			pattern: /<[^\s].*\/>/,
-			greedy: true,
-		},
 		'boolean': /\b(?:true|false|null)\b/,
 		'string': {
 			pattern: /"[^\n"]*"|'[^\n']*'/, // "
