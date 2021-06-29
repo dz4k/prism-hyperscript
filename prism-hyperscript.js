c52e6d5dfb9938a08a3c478887851384dc166bb0
Deniz Akşimşek <deniz@denizaksimsek.com>
2021-06-29 20:33:24 +0000

    add missing syntax

--- prism-hyperscript.js
+++ prism-hyperscript.js
@@ -16,10 +16,20 @@
 		},
 		'attribute': {
 			pattern: /@[a-zA-Z\-\_]+/,
+			inside: {
+				'attr-name': /[a-zA-Z\-\_].*/,
+			}
 		},
 		'attribute-ref': {
-			pattern: /\[@[a-zA-Z\-\_]="[^\n"]*"|'[^\n']*'\]/,    //"
+			pattern: /\[@[a-zA-Z\-\_]=(?:"[^\n"]*"|'[^\n']*')\]/,    //"
 			alias: 'selector',
+			inside: {
+				'attr-name': {
+					pattern: /(\^[@)[^=]*/,
+					lookbehind: true,
+				},
+				'attr-value': /"[^\n"]*"|'[^\n']*'/,                 //"
+			}
 		},
 		'class-ref': {
 			pattern: /\s\.[\-\w\d_\$]+/,
@@ -36,11 +46,30 @@
 			greedy: true,
 		},
 		'keyword': {
-			pattern: /\b(?:on|def|js|worker|eventsource|socket|init|behavior|install|catch|add|async|call|get|hide|measure|if|else|js|log|put|remove|repeat\sforever|repeat\sfor|repeat\sin|repeat\swhile|repeat\suntil\sevent|repeat until|repeat|return|send|settle|set|show|take|throw|toggle\sbetween|toggle|transition|trigger|wait for|wait|fetch|tell|go|make an|make a|make|then|end|while|until|for|in|from|to|with|over|into|before|after|at end of|at start of|is an|is a|is not|is|am|as|and|or|no|closest|the|of|first|last|random|local|element|global|on|seconds|milliseconds|(\s)s|(\s)ms)\b/g,
+			pattern: new RegExp(`
+\\b(?:on|def|js|worker|eventsource|socket|init|behavior|install|
+catch|add|async|call|get|hide|measure|if|else|js|log|put|remove|
+repeat forever|repeat for|repeat in|repeat while|
+repeat until event|repeat until|repeat|return|send|settle|set|
+show|take|throw|toggle between|toggle|transition|trigger|
+wait for|wait|fetch|tell|go|increment|decrement|halt|make an|
+make a|make|then|end|while|until|for|in|from|to|with|over|into|
+before|after|at end of|at start of|is an|is a|is not|is|am|as|
+and|or|no|closest|the|of|first|last|random|local|element|global|
+on|seconds|milliseconds|(\s)s|(\s)ms)
+`.split('\n').join(''), 'g'),
 			lookbehind: true,
 			inside: {
 				'hs-start': {
-					pattern: /\b(?:on|def|js|worker|eventsource|socket|init|behavior|install|catch|add|async|call|get|hide|measure|if|else|js|log|put|remove|repeat\sforever|repeat\sfor|repeat\sin|repeat\swhile|repeat\suntil\sevent|repeat\suntil|repeat|return|send|set|settle|show|take|throw|toggle\sbetween|toggle|transition|trigger|wait for|wait|fetch|tell|go|make|end|for)\b/g,
+					pattern: new RegExp(`
+^(?:on|def|js|worker|eventsource|socket|init|behavior|install|
+catch|add|async|call|get|hide|measure|if|else|js|log|put|remove|
+repeat forever|repeat for|repeat in|repeat while|
+repeat until event|repeat until|repeat|return|send|set|settle|
+show|take|throw|toggle between|toggle|transition|trigger|
+wait for|wait|fetch|tell|go|increment|decrement|halt|end|for)$|
+^make
+`.split('\n').join(''), 'g'),
 					alias: 'bold',
 				}
 			}
